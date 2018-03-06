75931d215d15a24ce8037454f302d23a19f99f51
Austin Wolfe <wolfe_austin@yahoo.com>
2018-03-06 00:09:09 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -13,3 +13,16 @@ end
 def pop_with_args(arr)
   arr.pop(2)
 end 
+
+def using_shift(arr)
+  arr.shift
+end 
+
+def shift_with_args(arr)
+  arr.shift(2)
+end 
+
+def using_concat(arr1, arr2)
+  arr1.concat(arr2)
+end 
+  
