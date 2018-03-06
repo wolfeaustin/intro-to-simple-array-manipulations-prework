02bf42bb3591cbbdad1d2898a7b9c29f8352fb1c
Austin Wolfe <wolfe_austin@yahoo.com>
2018-03-06 00:06:46 +0000

    automatically backed up by learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,15 @@
+def using_push(arr, string)
+  arr.push(string)
+end 
+
+def using_unshift(arr, string)
+  arr.unshift(string)
+end 
+
+def using_pop(arr)
+  arr.pop
+end 
+
+def pop_with_args(arr)
+  arr.pop(2)
+end 
